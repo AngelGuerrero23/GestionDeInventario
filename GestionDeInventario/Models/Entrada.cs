0f33241277f459bc10ca779c889d16abd07c0663
angel <angel_guerrero1@ucne.edu.do>
2026-01-04 22:02:23 -0400

    Add project files.

---
+++ GestionDeInventario/Models/Entrada.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionDeInventario.Models;
+
+public class Entrada
+{
+    [Key]
+    public int EntradaId { get; set; }
+    [Required(ErrorMessage = "La fecha de la entrada es obligatoria.")]
+    public DateTime Fecha { get; set; }
+    [Required(ErrorMessage = "El concepto de la entrada es obligatorio.")]
+    public string Concepto { get; set; }
+    [Range(0.01, int.MaxValue, ErrorMessage ="El Total debe de ser mayor a 0.01")]
+    public double Total { get; set; }
+
+}
---
+++ GestionDeInventario/Models/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionDeInventario.Models;
+
+public class Producto
+{
+    [Key]
+    public int ProductoId { get; set; }
+    [Required(ErrorMessage = "El nombre del producto es obligatorio.")]
+    public string Descripcion { get; set; } 
+    [Required(ErrorMessage = "El costo del producto es obligatorio.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "El costo debe ser mayor que cero.")]
+    public double Costo { get; set; }
+    [Required(ErrorMessage = "El precio del producto es obligatorio.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor que cero.")]
+    public decimal Precio { get; set; }
+    [Range(0, int.MaxValue, ErrorMessage = "La existencia no puede ser negativa.")]
+    public int Existencia { get; set; }
+}
