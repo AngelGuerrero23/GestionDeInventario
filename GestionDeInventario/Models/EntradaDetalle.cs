73e95ea281a536fde0f29a321688be4723751178
angel <angel_guerrero1@ucne.edu.do>
2026-01-04 22:10:23 -0400

    Creacion de Modelos

---
+++ GestionDeInventario/Models/EntradaDetalle.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestionDeInventario.Models;
+
+public class EntradaDetalle
+{
+    [Key]
+    public int Id { get; set; }
+    public int EntradaId { get; set; }
+    public int ProductoId { get; set; }
+    public int Cantidad { get; set; }
+    public double Costo { get; set; }
+
+    [ForeignKey("ProductoId")]
+    public virtual ICollection<Producto> Productos { get; set; }
+
+    [ForeignKey("EntradaId")]
+    public virtual ICollection<Entrada> Entradas { get; set; }
+
+}
